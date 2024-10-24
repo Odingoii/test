db352aab0414ad4242f3961f81a11413ece79e40
Soloh Odingo <124275153+Odingoii@users.noreply.github.com>
2024-10-24 17:09:24 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode'); // Use the 'qrcode' library for image generation
+const fs = require('fs');
 
 const client = new Client();
 
@@ -8,7 +9,11 @@ client.on('ready', () => {
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    // Generate and save QR code as a PNG image
+    qrcode.toFile('whatsapp-qr.png', qr, function (err) {
+        if (err) throw err;
+        console.log('QR code saved as whatsapp-qr.png');
+    });
 });
 
 client.initialize();
